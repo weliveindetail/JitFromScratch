971eb8905faaddd203b205f40b23c6c8b257a9aa
stefan <stefan.graenitz@gmail.com>
2017-08-25 17:05:00 +0200

    Replace manual codegen with Clang invokation

--- JitFromScratch.cpp
+++ JitFromScratch.cpp
@@ -25,78 +25,6 @@ LLVM_ATTRIBUTE_NORETURN static void fatalError(llvm::Error E) {
   exit(1);
 }
 
-llvm::Expected<std::string> codegenIR(llvm::Module *module, unsigned items) {
-  using namespace llvm;
-
-  LLVMContext &ctx = module->getContext();
-  IRBuilder<> Builder(ctx);
-
-  auto name = "integerDistance";
-  auto intTy = Type::getInt32Ty(ctx);
-  auto intPtrTy = intTy->getPointerTo();
-  auto signature = FunctionType::get(intPtrTy, {intPtrTy, intPtrTy}, false);
-  auto linkage = Function::ExternalLinkage;
-
-  auto fn = Function::Create(signature, linkage, name, module);
-  fn->setName(name); // so the CompileLayer can find it
-
-  Builder.SetInsertPoint(BasicBlock::Create(ctx, "entry", fn));
-  {
-    auto argIt = fn->arg_begin();
-    Argument &argPtrX = *argIt;
-    Argument &argPtrY = *(++argIt);
-
-    argPtrX.setName("x");
-    argPtrY.setName("y");
-
-#if _WIN32
-    const char *allocMangledName = "?customIntAllocator@@YAPEAHI@Z";
-#else
-    const char *allocMangledName = "_Z18customIntAllocatorj";
-#endif
-
-    auto allocSig = FunctionType::get(intPtrTy, {intTy}, false);
-    Value *allocFunction =
-        module->getOrInsertFunction(allocMangledName, allocSig);
-
-    Value *results_size = ConstantInt::get(intTy, items);
-    Value *results_ptr = Builder.CreateCall(allocFunction, {results_size});
-
-    auto absSig = FunctionType::get(intTy, {intTy}, false);
-    Value *absFunction = module->getOrInsertFunction("abs", absSig);
-
-    for (unsigned int i = 0; i < items; i++) {
-      Value *xi_ptr = Builder.CreateConstInBoundsGEP1_32(intTy, &argPtrX, i);
-      Value *yi_ptr = Builder.CreateConstInBoundsGEP1_32(intTy, &argPtrY, i);
-
-      Value *x0 = Builder.CreateLoad(xi_ptr);
-      Value *y0 = Builder.CreateLoad(yi_ptr);
-      Value *difference = Builder.CreateSub(x0, y0);
-      Value *absDifference = Builder.CreateCall(absFunction, {difference});
-
-      Value *ri_ptr = Builder.CreateConstInBoundsGEP1_32(intTy, results_ptr, i);
-      Builder.CreateStore(absDifference, ri_ptr);
-    }
-
-    Builder.CreateRet(results_ptr);
-  }
-
-  std::string error;
-  raw_string_ostream es(error);
-
-  if (verifyFunction(*fn, &es))
-    return make_error<StringError>(
-        Twine("Function verification failed:\n", es.str()),
-        llvm::inconvertibleErrorCode());
-
-  if (verifyModule(*module, &es))
-    return make_error<StringError>(
-        Twine("Module verification failed:\n", es.str()),
-        llvm::inconvertibleErrorCode());
-
-  return name;
-}
-
 // Determine the size of a C array at compile-time.
 template <typename T, size_t sizeOfArray>
 constexpr unsigned arrayElements(T (&)[sizeOfArray]) {
@@ -138,24 +66,37 @@ int main(int argc, char **argv) {
   auto targetMachine = EngineBuilder().selectTarget();
   auto jit = std::make_unique<SimpleOrcJit>(*targetMachine);
 
-  LLVMContext context;
-  auto module = std::make_unique<Module>("JitFromScratch", context);
-  module->setDataLayout(targetMachine->createDataLayout());
+  // Implementation of the integerDistances function.
+  std::string sourceCode =
+      "extern \"C\" int abs(int);                                           \n"
+      "extern int *customIntAllocator(unsigned items);                      \n"
+      "                                                                     \n"
+      "extern \"C\" int *integerDistances(int* x, int *y, unsigned items) { \n"
+      "  int *results = customIntAllocator(items);                          \n"
+      "                                                                     \n"
+      "  for (int i = 0; i < items; i++) {                                  \n"
+      "    results[i] = abs(x[i] - y[i]);                                   \n"
+      "  }                                                                  \n"
+      "                                                                     \n"
+      "  return results;                                                    \n"
+      "}                                                                    \n";
 
-  // Generate LLVM IR for the function.
-  Expected<std::string> jitedFnName = codegenIR(module.get(), arrayElements(x));
-  if (!jitedFnName)
-    fatalError(jitedFnName.takeError());
+  // Compile C++ to bitcode.
+  LLVMContext context;
+  auto module = jit->compileModuleFromCpp(sourceCode, context);
+  if (!module)
+    fatalError(module.takeError());
 
   // Compile to machine code and link.
-  jit->submitModule(std::move(module));
-  auto jitedFn = jit->getFunction<int *(int *, int *)>(*jitedFnName);
+  jit->submitModule(std::move(*module));
+  auto jitedFn =
+      jit->getFunction<int *(int *, int *, unsigned)>("integerDistances");
   if (!jitedFn)
     fatalError(jitedFn.takeError());
 
   // Invoke JITed function.
   auto integerDistances = *jitedFn;
-  int *z = integerDistances(x, y);
+  int *z = integerDistances(x, y, arrayElements(x));
 
   outs() << "Integer Distances: ";
   outs() << z[0] << ", " << z[1] << ", " << z[2] << "\n\n";
