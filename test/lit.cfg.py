56ef50113411a6fa4f3d7ed98b0d7325fbe4ad9e
stefan <stefan.graenitz@gmail.com>
2019-03-04 18:32:06 +0100

    In debug mode dump extra info when passing -debug -debug-only=jitfromscratch

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -10,6 +10,9 @@ config.test_format = lit.formats.ShTest()
 config.test_source_root = os.path.dirname(__file__)
 config.suffixes = ['.test']
 
+if config.build_type.lower() == "debug":
+  config.suffixes.append('.test-debug')
+
 # Add binary directories for JitFromScratch and FileCheck executables
 llvm_config.with_environment('PATH', config.jitfromscratch_build_dir, append_path=True)
 llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
