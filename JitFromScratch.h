aa2233a201a0c3ceb7475f833493fabf0fd50517
stefan <stefan.graenitz@gmail.com>
2019-03-04 19:13:54 +0100

    Request and run trivial function

--- JitFromScratch.h
+++ JitFromScratch.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <llvm/ADT/StringRef.h>
 #include <llvm/ADT/Triple.h>
+#include <llvm/ExecutionEngine/JITSymbol.h>
 #include <llvm/ExecutionEngine/Orc/LLJIT.h>
 #include <llvm/IR/DataLayout.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/Error.h>
 
+#include <functional>
 #include <memory>
 
 class JitFromScratch {
@@ -29,7 +32,19 @@ public:
 
   llvm::Error submitModule(std::unique_ptr<llvm::Module> M,
                            std::unique_ptr<llvm::LLVMContext> C);
+
+  template <class Signature_t>
+  llvm::Expected<std::function<Signature_t>> getFunction(llvm::StringRef Name) {
+    if (auto A = getFunctionAddr(Name))
+      return std::function<Signature_t>(
+          llvm::jitTargetAddressToPointer<Signature_t *>(*A));
+    else
+      return A.takeError();
+  }
+
 private:
   std::unique_ptr<llvm::orc::LLJIT> LLJIT;
   llvm::Triple TT;
+
+  llvm::Expected<llvm::JITTargetAddress> getFunctionAddr(llvm::StringRef Name);
 };
